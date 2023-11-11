082981a00d99b74cf37efd448338149f50cdb07f
pisces7diana <email7diana@gmail.com>
2023-11-11 15:51:09 -0800

    Easy Going answered

--- answers.js
+++ answers.js
@@ -1,1 +1,7 @@
-console.log(1);
+// Part 1: HTML & CSS //
+    // 3 - I almost feel like I've got it //
+
+// Part II: Javascript Reps //
+    // Easy Going //
+for (let i = 1 ; i < 21; i++)
+    console.log(i)
---
+++ draft.js
@@ -0,0 +1,24 @@
+ // Fizz Buzz //
+// 1.
+// 2.
+// 3.
+// 4.
+
+    // Wild Wild Wife //
+    const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+    const sharky = ["Sharky", "shark", 20, "Left Coast"]
+    const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+    const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+    const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001;
+console.log(plantee);
+
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+
+dart[4] = "Hawkins"
+console.log(dart);
+
+wolfy[0] = "Gameboy";
+console.log(wolfy);
