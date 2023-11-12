956dfd006c0eaf50e4c329489e65489123a5b22e
pisces7diana <email7diana@gmail.com>
2023-11-11 16:01:52 -0800

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -9,3 +9,28 @@ for (let i = 1 ; i < 21; i++)
     // Get Even //
 for (let i = 0; i < 201; i+=2)
     console.log(i);
+
+    // Fizz Buzz //
+// 1.
+// 2.
+// 3.
+// 4.
+
+    // Wild Wild Life //
+    const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+    const sharky = ["Sharky", "shark", 20, "Left Coast"]
+    const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+    const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+    const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001;
+console.log(plantee);
+
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+
+dart[4] = "Hawkins"
+console.log(dart);
+
+wolfy[0] = "Gameboy";
+console.log(wolfy);
